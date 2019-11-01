89f755b76425edca4f9b270191c808659118a690
MaxvanderWeide <43135550+MaxvanderWeide@users.noreply.github.com>
2019-11-01 16:10:18 +0100

    Beautified python DAT-1178

--- test/eac.sh
+++ test/eac.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 strep=$(gcloud app logs read --limit=1000 | grep "INFO:auditlog.*Url: \(https://.*\) .*e2e-technical-user" | cut -d'|' -f 1 | cut -d" " -f2,7 | cut -d'/' -f1,4- | sed 's/https://' | sed 's/ /|/')
 api=$(curl -s 'https://'"$2"'.appspot.com/openapi.json' | (python -c "
-import sys, json
-accepted_keys=''
-for key,item in list(json.load(sys.stdin)['paths'].items()):if'x-eac-ignore'not in item.keys():accepted_keys+=key+' '
+import sys
+import json
+accepted_keys = ''
+for key, item in list(json.load(sys.stdin)['paths'].items()):
+	if 'x-eac-ignore' not in item.keys():
+		accepted_keys += key + ' '
 print(accepted_keys)"))
 touch requests.txt;touch specs.txt
 for spec in $api;do echo "$spec">>specs.txt;done
