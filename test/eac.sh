4257f7f6420fb074f021e22eaa569a936e173713
MaxvanderWeide <43135550+MaxvanderWeide@users.noreply.github.com>
2019-11-04 10:39:16 +0100

    Switched true/false DAT-1179

--- test/eac.sh
+++ test/eac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 strep=$(gcloud app logs read --limit=1000 | grep "INFO:auditlog.*Url: \(https://.*\) .*e2e-technical-user" | cut -d'|' -f 1 | cut -d" " -f2,7 | cut -d'/' -f1,4- | sed 's/https://' | sed 's/ /|/')
-api=$(curl -s 'https://'"$2"'.appspot.com/openapi.json' | (python3 -c "
+api=$(curl -s 'https://'"$2"'.appspot.com/openapi.json' | (python -c "
 import sys
 import json
 accepted_keys = ''
@@ -12,6 +12,6 @@ touch requests.txt;touch specs.txt
 for spec in $api;do echo "$spec">>specs.txt;done
 for req in $strep
 do if [[ "$(echo "$req" | cut -d'|' -f1)" > $1 ]];then echo "$req"|cut -d'|' -f2>>requests.txt;fi;done
-script=$(python3 eac.py requests.txt specs.txt)
+script=$(python eac.py requests.txt specs.txt)
+if [ $? -eq 0 ];then echo "EAC PASSED: All endpoints were tested";else echo "EAC FAILED: Not all endpoints were tested";exit 1;fi 
 rm requests.txt;rm specs.txt
-if [[ $script != 1 ]];then echo "EAC FAILED: Not all endpoints were tested";exit 1;else echo "EAC PASSED: All endpoints were tested";exit 0;fi
