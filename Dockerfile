786a8fd2283229673dd4e6777445c8133ab01eef
MaxvanderWeide <max.vanderweide@student.hu.nl>
2020-07-03 13:14:54 +0200

    Added requests from file DAT-3593

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM python:3.7-slim
 WORKDIR /workspace
 
 RUN pip install virtualenv
+RUN pip install --upgrade google-cloud
 COPY docker-eac.sh /usr/local/bin/
-ADD coverage /
+COPY coverage /coverage
 ENTRYPOINT ["docker-eac.sh"]
--- docker-eac.sh
+++ docker-eac.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 set -eo pipefail
-if [ "$BRANCH_NAME" == "master" ]; then
+if [ "$2" == "master" ]; then
    echo "Skipping e2e api coverage test in master branch"
    exit 0
 fi
-if [ ! -f /start_datetime ]; then
+if [ ! -f "$3" ]; then
     echo "The start_datetime needs to be passed"
     exit 1
 fi
-python /coverage/main.py "${PROJECT_ID}" "$(</start_datetime)"
+
+python /coverage/main.py "$1" "$(<"$3")"
